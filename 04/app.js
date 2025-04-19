d7abca1c536d8115bc2fe34b3c59a03b9e84ead7
Neil Gorski <neil.gorski@hotmail.ch>
2025-04-19 13:17:09 +0200

    04-done

--- 04/app.js
+++ 04/app.js
@@ -1,7 +1,19 @@
-// 04.01
 const divList = document.querySelectorAll("div");
+const addClassClicked = function (e) {
+  const delay = Number(this.dataset.time);
+  setTimeout(() => this.classList.add("clicked"), delay);
+};
+
 for (div of divList) {
-  div.addEventListener("click", function (e) {
-    this.classList.add("clicked");
-  });
+  div.addEventListener("click", addClassClicked, true);
 }
+
+const body = document.querySelector("body");
+
+const removeDivClassClicked = function (e) {
+  if (e.target === body) {
+    divList.forEach((element) => element.classList.remove("clicked"));
+  }
+};
+
+body.addEventListener("click", removeDivClassClicked);
